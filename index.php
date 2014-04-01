df3a64a10558f593c34114b83f0a90ea0ae05239
Rik <gitaarik@gmail.com>
2014-04-02 00:18:14 +0200

    added meta tag for scaling on mobile devices

--- index.php
+++ index.php
@@ -301,8 +301,11 @@ $order_css_classes = get_order_css_classes($order_keys, $order_active_key, $orde
 
     <head>
 
-        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <title>Index of <?=$dirname?></title>
+
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+
         <style type="text/css">
 
             html, body {
